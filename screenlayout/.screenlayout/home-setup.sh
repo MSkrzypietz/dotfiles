d55641cf045a1c60494a0de6f424b8ac72905dd2
MSkrzypietz <michael.skr97@gmail.com>
2022-10-31 22:24:17 +0100

    add screen setup on i3 startup

---
+++ screenlayout/.screenlayout/home-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate right --output DP-0 --mode 2560x1440 --pos 4000x732 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --pos 1440x732 --rotate normal --output DP-3 --off --output HDMI-1 --off --output USB-C-0 --off
