08dcc0b1a156a6e3eee3a41b92880e2bc9e1465b
MSkrzypietz <michael.skr97@gmail.com>
2023-02-21 21:03:56 +0100

    add comment plugin

--- nvim/.config/nvim/lua/mskrzypietz/packer.lua
+++ nvim/.config/nvim/lua/mskrzypietz/packer.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
   use 'ThePrimeagen/harpoon'
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
+  use 'numToStr/Comment.nvim'
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
