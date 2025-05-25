926a61ab70d91dee58429259e52e7da4f2491358
MSkrzypietz <michael.skr97@gmail.com>
2025-05-25 16:17:35 +0200

    update dotfiles

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,54 +1,54 @@
-local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
-
-lsp.ensure_installed({
-	'tsserver',
-	'rust_analyzer'
-})
-
+-- local lsp = require('lsp-zero')
 --
--- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+-- lsp.preset('recommended')
 
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	--['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
-})
+-- lsp.ensure_installed({
+-- 	'tsserver',
+-- 	'rust_analyzer'
+-- })
+--
+--
+-- -- Fix Undefined global 'vim'
+-- lsp.configure('lua-language-server', {
+--     settings = {
+--         Lua = {
+--             diagnostics = {
+--                 globals = { 'vim' }
+--             }
+--         }
+--     }
+-- })
 
-lsp.set_preferences({
-	sign_icons = { }
-})
+-- local cmp = require('cmp')
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+-- 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+-- 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+-- 	--['<C-y>'] = cmp.mapping.confirm({ select = true }),
+-- 	['<C-Space>'] = cmp.mapping.complete(),
+-- })
+--
+-- lsp.set_preferences({
+-- 	sign_icons = { }
+-- })
+--
+-- lsp.setup_nvim_cmp({
+-- 	mapping = cmp_mappings
+-- })
+--
+-- lsp.on_attach(function(client, bufnr)
+-- 	local opts = { buffer = bufnr }
+-- 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+-- 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+-- 	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+-- 	vim.keymap.set('n', '<leader>of', vim.diagnostic.open_float, opts)
+-- 	vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+-- 	vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+-- 	vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, opts)
+-- 	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+-- 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+-- 	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+-- 	vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, {})
+-- end)
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-})
-
-lsp.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr }
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-	vim.keymap.set('n', '<leader>of', vim.diagnostic.open_float, opts)
-	vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
-	vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
-	vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, opts)
-	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-	vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, {})
-end)
-
-lsp.setup()
+-- lsp.setup()
